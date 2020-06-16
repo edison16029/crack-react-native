7b93c37587135613a0d47a144799fbe11e30ac10
Edison M <edison16029@gmail.com>
2020-06-16 11:35:37 +0530

    [Ed] Added and Tested Base Styles

---
+++ styles/base.js
@@ -0,0 +1,46 @@
+import { Dimensions } from 'react-native'
+
+export const dimensions = {
+    fullHeight: Dimensions.get('window').height,
+    fullWidth: Dimensions.get('window').width
+}
+    
+export const deafultColors  = {
+    primary: '#52575C',
+    primaryDark: '#353535',
+    accent: '#FFFFFF',
+}
+
+export const padding = {
+    xs : 5,
+    sm: 8,
+    md: 16,
+    lg: 32,
+    xl: 40
+}
+
+export const margin = {
+    xs : 5,
+    sm: 8,
+    md: 16,
+    lg: 32,
+    xl: 40
+}
+
+export const fonts = {
+    sm: 18,
+    md: 24,
+    lg: 36,
+    spacingSm: 15,
+    spacingLg: 20,
+    primary: 'Roboto'
+}
+
+export const borderRadius = {
+    radiusSm : 8,
+    radiusMd : 16,
+    radiusLg : 24,
+} 
+
+
+
